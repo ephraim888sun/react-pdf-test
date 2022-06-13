3dab00ede92534bc74be3858aa906be1366c5a83
Ephraim <ephraim888sun@gmail.com>
2022-06-13 14:57:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Scout from './ScoutEP_Logo2.png'
+import generatePDF from './PdfObject'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={() => generatePDF()}>Generate PDF</button>
+      {/* {generatePDF()} */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
